c7a932eddbe0b8024dc810d439732e9a9bec1365
“ahonn” <ahonn95@outlook.com>
2016-06-23 23:59:10 +0800

    addd reply

--- src/views/topic.vue
+++ src/views/topic.vue
@@ -10,10 +10,23 @@
       <img :src="topic.member.avatar_normal" />
       <div class="col">
         <span class="author" v-text="topic.member.username" ></span>
-        <time> 发布于：{{ topic.created | getTimeStr true }}</time>
+        <time>发布于：{{ topic.created | getTimeStr true }}</time>
       </div>
     </section>
     <section class="topic-content" v-html="topic.content_rendered">
+    </section>
+    <section class="replies">
+      <ul class="replies_list">
+        <li v-for="reply in replies" class="reply">
+          <img :src="reply.member.avatar_mini" />
+          <div class="col">
+            <span class="username" v-text="reply.member.username"></span>
+            <time v-text="reply.created | getTimeStr true"></time>
+          </div>
+          <div class="reply-content" v-html="reply.content_rendered">
+          </div>
+        </li>
+      </ul>
     </section>
   </section>
 </template>
@@ -23,7 +36,8 @@
     data () {
       return {
         showMenu: false,
-        topic: {}
+        topic: {},
+        replies: {}
       }
     },
     route: {
@@ -36,13 +50,19 @@
     },
     methods: {
       getTopic (topicId) {
-        let url = './api/topics/' + topicId;
+        this.$http.get('./api/topics/' + topicId)
+          .then(function (response) {
+            if (response.data) {
+              this.topic = response.data[0];
+            }
+          });
 
-        this.$http.get(url).then(function (response) {
-          if (response.data) {
-            this.topic = response.data[0];
-          }
-        });
+        this.$http.get('./api/replies/' + topicId)
+          .then(function (response) {
+            if (response.data) {
+              this.replies = response.data;
+            }
+          });
       }
     },
     components: {
@@ -54,6 +74,7 @@
 <style lang="sass">
   #page {
     padding-top: 44px;
+    font-size: 14px;
 
     .topic-title {
       margin: 15px;
@@ -70,13 +91,13 @@
       img {
         width: 40px;
         height: 40px;
-        margin-right: 10px;
         border-radius: 50%;
         border: 1px solid #f0f0f0;
       }
 
       .col {
         display: inline-block;
+        margin-left: 10px;
         position: relative;
         top: -5px;
 
@@ -98,6 +119,47 @@
 
       img {
         width: 100%;
+      }
+
+      code {
+        display: block;
+        overflow-x: auto;
+      }
+    }
+
+    .replies_list {
+      list-style: none;
+      padding-left: 0px;
+
+      .reply {
+        padding: 15px;
+        border-top: 1px solid #ddd;
+
+        img {
+          width: 20px;
+          height: 20px;
+          border-radius: 50%;
+          border: 1px solid #f0f0f0;
+        }
+
+        .col {
+          display: inline-block;
+          margin-left: 5px;
+          position: relative;
+          top: -5px;
+
+          time {
+            margin-left: 5px;
+            color: #bbb;
+            font-size: 12px;
+          }
+        }
+
+        .reply-content {
+          margin-top: 5px;
+          word-break: break-all;
+          word-wrap: break-woed;
+        }
       }
     }
   }

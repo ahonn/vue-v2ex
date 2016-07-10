cb3506e09268c0bb41fd8f17b1e0940481a13a54
ahonn <ahonn95@outlook.com>
2016-07-10 18:08:40 +0800

    add topics list

--- src/components/items.vue
+++ src/components/items.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="topics">
     <div class="topic-item" v-for="topic in topics">
-      <span v-text="topic.title"></span>
+      <div class="avatar">
+        <img :src="topic.member.avatar_normal" :alt="topic.member.username" />
+      </div>
+      <div class="content">
+        <h3 v-text="topic.title"></h3>
+        <div class="info">
+          <span class="node" v-text="topic.node.title"></span>
+          <span class="author" v-text="topic.member.username"></span>
+          <span class="created" v-text="topic.created "></span>
+          <span class="replies" v-text="topic.replies"></span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -19,11 +30,56 @@
 
 <style lang="less"> 
   .topics {
-    margin: 10px 15px 65px;
+    margin: 0px 15px 65px;
 
     .topic-item {
-      padding: 10px 0px;
+      padding: 15px 0px;
       border-bottom: 1px #ddd solid; 
+
+      .avatar {
+        float: left;
+        padding-right: 15px;
+
+        img {
+          width: 40px;
+          border-radius: 50%;
+        }
+      }
+
+      .content {
+          
+        h3 {
+          margin: 0;
+          font-size: 15px;
+          line-height: 1.5;
+          white-space:nowrap;
+          text-overflow: ellipsis;
+          overflow: hidden;
+        }
+
+        .info {
+          color: #888;
+          margin-top: 5px;
+          font-size: 12px;
+
+          .author {
+            font-size: 13px;
+          }
+
+          .node::after,
+          .author::after {
+            content: "-";
+            margin-left: 3px;
+          }
+
+          .replies {
+            float: right;
+            padding: 0px 8px;
+            background-color: #ddd;
+            border-radius: 10px;
+          }
+        }
+      }
     }
   
   }
